de4d325639e97ab5fa14c3cd4956b44479d69ca9
rinogodson <rinogodson@gmail.com>
2025-07-03 17:38:12 +0530

    improvements

--- react/src/components/ColorChangeOption/ColorChangeOption.tsx
+++ react/src/components/ColorChangeOption/ColorChangeOption.tsx
@@ -1,10 +1,104 @@
+import { Droplet, Palette } from "lucide-react";
+import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+
 function ColorChangeOption() {
+  const [option, setOption] = useState({
+    isColor: true,
+    isTitle: true,
+  });
+
+  const [inputValue, setInputValue] = useState({
+    color: "#3E3F76",
+    blur: "1",
+  });
+
+  const style = {
+    pillStyle:
+      "w-[10em] h-[3em] bg-black border-[1px_1px_2px_1px] border-[#262626] rounded-full flex items-center cursor-pointer justify-between mt-5 px-3.5",
+  };
+
   return (
-    <div className="flex flex-row self-start relative justify-self-start top-0">
-      <div className="w-[10em] h-[3em] bg-gray-900 rounded-full flex items-center justify-center mt-2 ml-2"> </div>
-      <div className="w-[10em] h-[3em] bg-gray-900 rounded-full flex items-center justify-center mt-2 ml-2"> </div>
+    <div className="flex flex-row self-start relative justify-self-start top-0 w-full justify-center gap-5">
+      <div className={style.pillStyle}>
+        <AnimatePresence mode="wait">
+          {option.isColor ? (
+            <motion.div
+              key="color"
+              initial={{ opacity: 0, x: -10 }}
+              animate={{ opacity: 1, x: 0 }}
+              exit={{ opacity: 0, x: -10 }}
+              transition={{ duration: 0.3 }}
+              className="text-[#FFF] text-[1.2em] font-semibold flex items-center gap-3"
+            >
+              <input
+                value={inputValue.color}
+                onChange={(e) =>
+                  setInputValue({ ...inputValue, color: e.target.value })
+                }
+                type="color"
+                style={{ backgroundColor: inputValue.color }}
+                className="aspect-square w-[1.8em] rounded-[0.7em] border-[0.1em] border-[rgba(255,255,255,0.1)] text-center cursor-pointer"
+              />
+              <p>Color</p>
+            </motion.div>
+          ) : (
+            <motion.div
+              key="palette"
+              initial={{ opacity: 0, x: -10 }}
+              animate={{ opacity: 1, x: 0 }}
+              exit={{ opacity: 0, x: -10 }}
+              transition={{ duration: 0.3 }}
+            >
+              <Palette
+                className="hover:scale-125 transition-all duration-200 cursor-pointer active:scale-95"
+                onClick={() =>
+                  setOption({ ...option, isColor: !option.isColor })
+                }
+              />
+            </motion.div>
+          )}
+          <div className="w-[0.1em] h-[80%] bg-[rgba(255,255,255,0.1)]"></div>
+          {!option.isColor ? (
+            <motion.div
+              key="blur"
+              initial={{ opacity: 0, x: 10 }}
+              animate={{ opacity: 1, x: 0 }}
+              exit={{ opacity: 0, x: 10 }}
+              transition={{ duration: 0.3 }}
+              className="text-[#FFF] text-[1.2em] font-semibold flex items-center gap-3"
+            >
+              <p>Blur</p>
+              <input
+                value={inputValue.blur}
+                onChange={(e) =>
+                  setInputValue({ ...inputValue, blur: e.target.value })
+                }
+                type="number"
+                className="aspect-square w-[1.8em] bg-[rgba(255,255,255,0.08)] rounded-[0.7em] border-[0.08em] border-[rgba(255,255,255,0.1)] text-center [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
+              />
+            </motion.div>
+          ) : (
+            <motion.div
+              key="droplet"
+              initial={{ opacity: 0, x: 10 }}
+              animate={{ opacity: 1, x: 0 }}
+              exit={{ opacity: 0, x: 10 }}
+              transition={{ duration: 0.3 }}
+            >
+              <Droplet
+                className="hover:scale-125 transition-all duration-200 cursor-pointer active:scale-95"
+                onClick={() =>
+                  setOption({ ...option, isColor: !option.isColor })
+                }
+              />
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </div>
+      <div className={style.pillStyle}> </div>
     </div>
-  )
+  );
 }
 
-export default ColorChangeOption
+export default ColorChangeOption;
